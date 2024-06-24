6b4caf38054e436cbf6d300bf3f555d5e6bad2b4
Lim Su Bin <subin802@naver.com>
2024-06-24 15:39:42 +0900

    네번째 커밋 나누기 추가

--- main.py
+++ main.py
@@ -2,13 +2,14 @@
 def add_num(n1, n2):
     return n1 + n2
 
-
 def sub_num(n1, n2):
     return n1 - n2
 
-
 def mul_num(n1, n2):
     return n1 * n2
+
+def div_num(n1, n2):
+    return n1 / n2
 
 
 ## 전역 변수부
@@ -19,14 +20,14 @@ num1, num2, res = 100, 200, 0
 res = add_num(num1,num2)
 print(num1, '+', num2, '=', res)
 
-
 res = sub_num(num1,num2)
 print(num1, '-', num2, '=', res)
 
-
 res = sub_num(num1,num2)
 print(num1, '-', num2, '=', res)
-
 
 res = mul_num(num1,num2)
 print(num1, '*', num2, '=', res)
+
+res = div_num(num1,num2)
+print(num1, '/', num2, '=', res)
